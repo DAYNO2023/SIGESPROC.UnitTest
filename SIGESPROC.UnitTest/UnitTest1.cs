d7c3f1435e789a26fdb5e49e975c4cf0da6c800a
Madian2112 <yordin32sanchez@gmail.com>
2024-09-23 11:48:44 -0600

    UnitTest

---
+++ SIGESPROC.UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using SIGESPROC.DataAccess.Repositories.RepositoryProyecto;
+
+namespace SIGESPROC.UnitTest
+{
+    public class Tests
+    {
+        private readonly PresupuestoEncabezadoRepository _presupuestoencabezadoRepository;
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
