082b6082dbe9e31a448c2b3094fff9ee01aa6821
Yordin Sanchez <146747575+DAYNO2023@users.noreply.github.com>
2024-09-23 20:29:11 -0600

    Prueba unitarias

---
+++ SIGESPROC.UnitTest/Controllers/ProsupuestoEmcabezadoTest.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SIGESPROC.API.Controllers.ControllersProyecto;
+using SIGESPROC.API.Extensions;
+using SIGESPROC.BusinessLogic.Services.ServiceProyecto;
+using SIGESPROC.Common.Models.ModelsProyecto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGESPROC.UnitTest.Controllers
+{
+    [TestClass]
+    public class ProsupuestoEmcabezadoTest
+    {
+        private ProyectoService _proyectoService;
+        private static IMapper _mapper;
+
+        public ProsupuestoEmcabezadoTest()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new MappingProfileExtensions());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+        protected Mock<IMapper> map = new Mock<IMapper>();
+        PresupuestoEncabezadoController _presupuestoEncabezado;
+
+        [TestInitialize]
+        public void Initalize()
+        {
+            _presupuestoEncabezado = new PresupuestoEncabezadoController(_proyectoService, _mapper);
+        }
+
+
+        [TestMethod]
+        public void presupuestoEncabezadoCreate()
+        {
+            var data = new PresupuestoEncabezadoViewModel();
+            var result = _presupuestoEncabezado.Create(data);
+            //int Code = (int)result.StatusCode;
+
+            Assert.IsInstanceOfType<IActionResult>(result);
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
